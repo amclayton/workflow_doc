32a481665280d0acb254e6f7003e4ba8c3f5b74e
Adam Clayton <adam@Adams-MacBook-Air.local>
2026-05-24 14:38:23 +0800

    Further initial files

---
+++ source/conf.py
@@ -0,0 +1,61 @@
+"""Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "workflow_doc"
+copyright = "[Copyright]"
+author = "[Authors]"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.doctest",
+    "sphinx.ext.extlinks",
+]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_baseurl = "https://amclayton.github.io/workflow_doc/"
+
+# -- LaTeX output configuration ----------------------------------------------
+# https://www.sphinx-doc.org/en/master/latex.html
+
+latex_engine = "xelatex"
+latex_elements = {"papersize": "a4paper"}
+latex_show_urls = "footnote"
+
+# -- Extlinks configuration --------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/extlinks.html
+
+#extlinks = {
+#    "issue": ("https://github.com/MetOffice/CSET/issues/%s", "Issue #%s"),
+#    "pr": ("https://github.com/MetOffice/CSET/pull/%s", "PR #%s"),
+#}
+
+# -- Linkcheck configuration -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-the-linkcheck-builder
+
+linkcheck_anchors = False
+linkcheck_ignore = [
+    # Ignore GitHub links as we have many and don't want to be rate limited.
+    r"https://github.com/.+/(issues|pull)/\d+",
+    # GNU.org has an aggressive rate limit.
+    r"https://www.gnu.org/software/make/",
+    # CSET-restricted-files is a private repository.
+#    r"https://github.com/MetOffice/CSET-restricted-files",
+    # StackOverflow blocks requests.
+    r"https://stackoverflow.com/.+",
+    # Sourceforge blocks requests.
+    r"https://.+\.sourceforge\.io/.+",
+]
